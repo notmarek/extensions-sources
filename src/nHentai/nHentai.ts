ec55a6c141ac9a82accd8529819c03b42624937c
Marek Veselý <veselym@spstrutnov.cz>
2021-08-15 12:31:43 +0200

    Rewrite nHentai module

---
+++ src/nHentai/nHentai.ts
@@ -0,0 +1,143 @@
+import {
+  Source,
+  Manga,
+  Chapter,
+  ChapterDetails,
+  HomeSection,
+  SearchRequest,
+  PagedResults,
+  SourceInfo,
+  TagType,
+  LanguageCode
+} from "paperback-extensions-common"
+import { parseChapterDetails, parseGallery, parseSearch } from "./nHentaiParser"
+
+
+const NHENTAI_URL = "https://nhentai.net"
+const API = NHENTAI_URL + "/api"
+const method = 'GET'
+
+export const nHentaiInfo: SourceInfo = {
+  version: "3.0.0",
+  name: "nHentai",
+  description: `Extension which pulls 18+ content from nHentai. (Literally all of it. We know why you're here)`,
+  author: `NotMarek`,
+  authorWebsite: `https://github.com/notmarek`,
+  icon: `icon.png`,
+  hentaiSource: true,
+  sourceTags: [{ text: "18+", type: TagType.YELLOW }],
+  websiteBaseURL: NHENTAI_URL,
+}
+
+export class nHentai extends Source {
+  async getMangaDetails(mangaId: string): Promise<Manga> {
+    const request = createRequestObject({
+      url: `${API}/gallery/${mangaId}`,
+      method,
+    })
+    const data = await this.requestManager.schedule(request, 1)
+    let json_data = JSON.parse(JSON.stringify(data.data))
+    return parseGallery(json_data)
+  }
+
+  async getChapters(mangaId: string): Promise<Chapter[]> {
+    let chapters: Chapter[] = [
+      createChapter({
+        id: "",
+        mangaId: mangaId,
+        chapNum: 1,
+        langCode: LanguageCode.ENGLISH,
+      })
+    ]
+    return chapters;
+  }
+
+  async getChapterDetails(mangaId: string, chapterId: string): Promise<ChapterDetails> {
+    const request = createRequestObject({
+      url: `${API}/gallery/${mangaId}`,
+      method
+    })
+    const data = await this.requestManager.schedule(request, 1)
+    let json_data = JSON.parse(JSON.stringify(data.data))
+    return parseChapterDetails(json_data, mangaId);
+
+  }
+
+  async searchRequest(query: SearchRequest, metadata: any): Promise<PagedResults> {
+    if (query.hStatus === false) {
+      return createPagedResults({
+        results: [],
+        metadata: {
+          page: 1
+        }
+      })
+    }
+    let page: number = metadata?.page ?? 1;
+    let title: string = query.title ?? "";
+    if (/^\d+$/.test(title) && title.length <= 6) {
+      const request = createRequestObject({
+        url: `${API}/gallery/${title}`,
+        method
+      })
+      const data = await this.requestManager.schedule(request, 1)
+      let json_data = JSON.parse(JSON.stringify(data.data))
+      return createPagedResults({
+        results: parseSearch({ result: [json_data] }),
+        metadata: {
+          page: page + 1
+        }
+      })
+    } else {
+      const request = createRequestObject({
+        url: `${API}/galleries/search?query=${encodeURIComponent(title + " \"english\"")}&sort=popular&page=${page}`,
+        method
+      })
+      const data = await this.requestManager.schedule(request, 1)
+      let json_data = JSON.parse(JSON.stringify(data.data))
+      return createPagedResults({
+        results: parseSearch(json_data),
+        metadata: {
+          page: page + 1
+        }
+      })
+    }
+
+  }
+
+  async getHomePageSections(sectionCallback: (section: HomeSection) => void): Promise<void> {
+    const section1 = createHomeSection({ id: 'date', title: 'Recent', view_more: true });
+    const section2 = createHomeSection({ id: 'popular-today', title: 'Popular Today', view_more: true });
+    const section3 = createHomeSection({ id: 'popular-week', title: 'Popular Week', view_more: true });
+    const section4 = createHomeSection({ id: 'popular', title: 'Popular All-time', view_more: true });
+    const sections = [section1, section2, section3, section4];
+
+    for (const section of sections) {
+      sectionCallback(section);
+      let request = createRequestObject({
+        url: `${API}/galleries/search?query=${encodeURIComponent("english")}&sort=${section.id}`,
+        method
+      })
+      const data = await this.requestManager.schedule(request, 1);
+      let json_data = JSON.parse(JSON.stringify(data.data))
+      section.items = parseSearch(json_data);
+      sectionCallback(section);
+    }
+  }
+
+  async getViewMoreItems(homepageSectionId: string, metadata: any): Promise<PagedResults> {
+    let page: number = metadata?.page ?? 1;
+    const request = createRequestObject({
+      url: `${API}/galleries/search?query=${encodeURIComponent("english")}&sort=${homepageSectionId}&page=${page}`,
+      method
+    })
+    const data = await this.requestManager.schedule(request, 1)
+    let json_data = JSON.parse(JSON.stringify(data.data))
+    page++;
+    return createPagedResults({
+      results: parseSearch(json_data),
+      metadata: {
+        page: page
+      }
+    })
+  }
+}
---
+++ src/nHentai/nHentaiParser.ts
@@ -0,0 +1,104 @@
+import {
+    Manga,
+    ChapterDetails,
+    MangaStatus,
+    MangaTile
+} from "paperback-extensions-common"
+
+export interface nHentaiImage {
+    t: string; // type
+    w: number; // width
+    h: number; // height
+}
+
+const typeOfImage = (image: nHentaiImage): string => {
+    let map: { [key: string]: string } = { "j": "jpg", "p": "png" };
+    return map[image.t];
+}
+
+export interface nHentaiImages {
+    pages: nHentaiImage[];
+    cover: nHentaiImage;
+    thumbnail: nHentaiImage;
+}
+
+export interface nHentaiTag {
+    id: number;
+    type: string;
+    name: string;
+    url: string;
+    count: number;
+}
+
+export interface GalleryTitle {
+    english: string;
+    japanese: string;
+    pretty: string;
+}
+
+export interface Gallery {
+    id: number;
+    media_id: string;
+    title: GalleryTitle;
+    images: nHentaiImages;
+    scanlator: string;
+    upload_date: number;
+    tags: nHentaiTag[];
+    num_pages: number;
+    num_favorites: number;
+}
+
+export interface nHentaiSearch {
+    result: Gallery[];
+}
+
+const getArtist = (gallery: Gallery): string => {
+    let tags: nHentaiTag[] = gallery.tags;
+    for (const tag of tags) {
+        if (tag.type === "artist") {
+            return tag.name;
+        }
+    }
+    return "";
+}
+
+export const parseGallery = (data: Gallery): Manga => {
+    return createManga({
+        id: data.id.toString(),
+        titles: [data.title.english, data.title.japanese, data.title.pretty],
+        artist: getArtist(data),
+        image: `https://t.nhentai.net/galleries/${data.media_id}/cover.${typeOfImage(data.images.cover)}`,
+        rating: 0,
+        status: MangaStatus.COMPLETED,
+        hentai: true,
+    })
+}
+
+export const parseChapterDetails = (data: Gallery, mangaId: string): ChapterDetails => {
+    let counter: number = 0;
+    return createChapterDetails({
+        id: mangaId,
+        mangaId: mangaId,
+        longStrip: false,
+        pages: data.images.pages.map(image => {
+            let type = typeOfImage(image);
+            counter++;
+            return `https://i.nhentai.net/galleries/${data.media_id}/${counter}.${type}`;
+        }),
+    })
+}
+
+export const parseSearch = (data: nHentaiSearch): MangaTile[] => {
+    const tiles: MangaTile[] = [];
+    for (let gallery of data.result) {
+        tiles.push(createMangaTile({
+            id: gallery.id.toString(),
+            image: `https://t.nhentai.net/galleries/${gallery.media_id}/cover.${typeOfImage(gallery.images.cover)}`,
+            title: createIconText({
+                text: gallery.title.english
+            })
+        }))
+    }
+    return tiles;
+}
+
